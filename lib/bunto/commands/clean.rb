bc030bc16ebb0f158f710012b2ecd15d5e99c3d0
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-03-29 12:24:00 +0200

    Create clean.rb

---
+++ lib/bunto/commands/clean.rb
@@ -0,0 +1,42 @@
+module Bunto
+  module Commands
+    class Clean < Command
+      class << self
+
+        def init_with_program(prog)
+          prog.command(:clean) do |c|
+            c.syntax 'clean [subcommand]'
+            c.description 'Clean the site (removes site output and metadata file) without building.'
+
+            c.action do |args, _|
+              Bunto::Commands::Clean.process({})
+            end
+          end
+        end
+
+        def process(options)
+          options = configuration_from_options(options)
+          destination = options['destination']
+          metadata_file = File.join(options['source'], '.bunto-metadata')
+
+          if File.directory? destination
+            Bunto.logger.info "Cleaning #{destination}..."
+            FileUtils.rm_rf(destination)
+            Bunto.logger.info "", "done."
+          else
+            Bunto.logger.info "Nothing to do for #{destination}."
+          end
+
+          if File.file? metadata_file
+            Bunto.logger.info "Removing #{metadata_file}..."
+            FileUtils.rm_rf(metadata_file)
+            Bunto.logger.info "", "done."
+          else
+            Bunto.logger.info "Nothing to do for #{metadata_file}."
+          end
+        end
+
+      end
+    end
+  end
+end
