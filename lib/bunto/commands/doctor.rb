d39e50b32945ef6985d1bb9ad2e67fd385716356
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-03-29 12:24:50 +0200

    Create doctor.rb

---
+++ lib/bunto/commands/doctor.rb
@@ -0,0 +1,84 @@
+module Bunto
+  module Commands
+    class Doctor < Command
+      class << self
+
+        def init_with_program(prog)
+          prog.command(:doctor) do |c|
+            c.syntax 'doctor'
+            c.description 'Search site and print specific deprecation warnings'
+            c.alias(:hyde)
+
+            c.option '--config CONFIG_FILE[,CONFIG_FILE2,...]', Array, 'Custom configuration file'
+
+            c.action do |args, options|
+              Bunto::Commands::Doctor.process(options)
+            end
+          end
+        end
+
+        def process(options)
+          site = Bunto::Site.new(configuration_from_options(options))
+          site.read
+
+          if healthy?(site)
+            Bunto.logger.info "Your test results", "are in. Everything looks fine."
+          else
+            abort
+          end
+        end
+
+        def healthy?(site)
+          [
+            !deprecated_relative_permalinks(site),
+            !conflicting_urls(site)
+          ].all?
+        end
+
+        def deprecated_relative_permalinks(site)
+          contains_deprecated_pages = false
+          site.pages.each do |page|
+            if page.uses_relative_permalinks
+              Bunto::Deprecator.deprecation_message "'#{page.path}' uses relative" +
+                                  " permalinks which will be deprecated in" +
+                                  " Bunto v1.0.0 and beyond."
+              contains_deprecated_pages = true
+            end
+          end
+          contains_deprecated_pages
+        end
+
+        def conflicting_urls(site)
+          conflicting_urls = false
+          urls = {}
+          urls = collect_urls(urls, site.pages, site.dest)
+          urls = collect_urls(urls, site.posts, site.dest)
+          urls.each do |url, paths|
+            if paths.size > 1
+              conflicting_urls = true
+              Bunto.logger.warn "Conflict:", "The URL '#{url}' is the destination" +
+                " for the following pages: #{paths.join(", ")}"
+            end
+          end
+          conflicting_urls
+        end
+
+        private
+
+        def collect_urls(urls, things, destination)
+          things.each do |thing|
+            dest = thing.destination(destination)
+            if urls[dest]
+              urls[dest] << thing.path
+            else
+              urls[dest] = [thing.path]
+            end
+          end
+          urls
+        end
+
+      end
+
+    end
+  end
+end
