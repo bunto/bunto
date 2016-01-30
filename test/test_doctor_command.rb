4b843fc4595d3b930bb0f2ca236f343c04d07c4c
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-01-30 13:58:53 +0100

    Create test_doctor_command.rb

---
+++ test/test_doctor_command.rb
@@ -0,0 +1,36 @@
+require 'helper'
+require 'bunto/commands/doctor'
+
+class TestDoctorCommand < BuntoUnitTest
+  context 'urls only differ by case' do
+    setup do
+      clear_dest
+    end
+
+    should 'return success on a valid site/page' do
+      @site = Site.new(Bunto.configuration({
+        "source" => File.join(source_dir, '/_urls_differ_by_case_valid'),
+        "destination" => dest_dir
+      }))
+      @site.process
+      output = capture_stderr do
+         ret = Bunto::Commands::Doctor.urls_only_differ_by_case(@site)
+         assert_equal false, ret
+      end
+      assert_equal "", output
+    end
+
+    should 'return warning for pages only differing by case' do
+      @site = Site.new(Bunto.configuration({
+        "source" => File.join(source_dir, '/_urls_differ_by_case_invalid'),
+        "destination" => dest_dir
+      }))
+      @site.process
+      output = capture_stderr do
+         ret = Bunto::Commands::Doctor.urls_only_differ_by_case(@site)
+         assert_equal true, ret
+      end
+      assert_includes output, "Warning: The following URLs only differ by case. On a case-insensitive file system one of the URLs will be overwritten by the other: #{dest_dir}/about/index.html, #{dest_dir}/About/index.html"
+    end
+  end
+end
