92eeac2ee2997ead4aa9d82f3f95fd3305b13ee5
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-02-06 16:51:24 +0100

    Update document.rb

--- lib/bunto/document.rb
+++ lib/bunto/document.rb
@@ -4,24 +4,39 @@ module Bunto
   class Document
     include Comparable
 
-    attr_reader   :path, :site, :extname, :output_ext
-    attr_accessor :content, :collection, :output
+    attr_reader :path, :site, :extname, :collection
+    attr_accessor :content, :output
 
     YAML_FRONT_MATTER_REGEXP = /\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m
+    DATELESS_FILENAME_MATCHER = /^(.+\/)*(.*)(\.[^.]+)$/
+    DATE_FILENAME_MATCHER = /^(.+\/)*(\d+-\d+-\d+)-(.*)(\.[^.]+)$/
 
     # Create a new Document.
     #
-    # site - the Bunto::Site instance to which this Document belongs
     # path - the path to the file
+    # relations - a hash with keys :site and :collection, the values of which
+    #             are the Bunto::Site and Bunto::Collection to which this
+    #             Document belong.
     #
     # Returns nothing.
-    def initialize(path, relations)
+    def initialize(path, relations = {})
       @site = relations[:site]
       @path = path
       @extname = File.extname(path)
-      @output_ext = Bunto::Renderer.new(site, self).output_ext
       @collection = relations[:collection]
       @has_yaml_header = nil
+
+      if draft?
+        categories_from_path("_drafts")
+      else
+        categories_from_path(collection.relative_directory)
+      end
+
+      data.default_proc = proc do |_, key|
+        site.frontmatter_defaults.find(relative_path, collection.label, key)
+      end
+
+      trigger_hooks(:post_init)
     end
 
     # Fetch the Document's data.
@@ -29,7 +44,40 @@ module Bunto
     # Returns a Hash containing the data. An empty hash is returned if
     #   no data was read.
     def data
-      @data ||= Hash.new
+      @data ||= {}
+    end
+
+    # Merge some data in with this document's data.
+    #
+    # Returns the merged data.
+    def merge_data!(other, source: "YAML front matter")
+      if other.key?('categories') && !other['categories'].nil?
+        if other['categories'].is_a?(String)
+          other['categories'] = other['categories'].split(" ").map(&:strip)
+        end
+        other['categories'] = (data['categories'] || []) | other['categories']
+      end
+      Utils.deep_merge_hashes!(data, other)
+      if data.key?('date') && !data['date'].is_a?(Time)
+        data['date'] = Utils.parse_date(
+          data['date'].to_s,
+          "Document '#{relative_path}' does not have a valid date in the #{source}."
+        )
+      end
+      data
+    end
+
+    def date
+      data['date'] ||= site.time
+    end
+
+    # Returns whether the document is a draft. This is only the case if
+    # the document is in the 'posts' collection but in a different
+    # directory than '_posts'.
+    #
+    # Returns whether the document is a draft.
+    def draft?
+      data['draft'] ||= relative_path.index(collection.relative_directory).nil? && collection.label == "posts"
     end
 
     # The path to the document, relative to the site source.
@@ -38,6 +86,13 @@ module Bunto
     #   from the site source to this document
     def relative_path
       @relative_path ||= Pathname.new(path).relative_path_from(Pathname.new(site.source)).to_s
+    end
+
+    # The output extension of the document.
+    #
+    # Returns the output extension
+    def output_ext
+      Bunto::Renderer.new(site, self).output_ext
     end
 
     # The base filename of the document, without the file extname.
@@ -67,14 +122,14 @@ module Bunto
     # Returns the cleaned relative path of the document.
     def cleaned_relative_path
       @cleaned_relative_path ||=
-        relative_path[0 .. -extname.length - 1].sub(collection.relative_directory, "")
+        relative_path[0..-extname.length - 1].sub(collection.relative_directory, "")
     end
 
     # Determine whether the document is a YAML file.
     #
     # Returns true if the extname is either .yml or .yaml, false otherwise.
     def yaml_file?
-      %w[.yaml .yml].include?(extname)
+      %w(.yaml .yml).include?(extname)
     end
 
     # Determine whether the document is an asset file.
@@ -90,7 +145,7 @@ module Bunto
     #
     # Returns true if extname == .sass or .scss, false otherwise.
     def sass_file?
-      %w[.sass .scss].include?(extname)
+      %w(.sass .scss).include?(extname)
     end
 
     # Determine whether the document is a CoffeeScript file.
@@ -128,13 +183,7 @@ module Bunto
     #
     # Returns the Hash of key-value pairs for replacement in the URL.
     def url_placeholders
-      {
-        collection: collection.label,
-        path:       cleaned_relative_path,
-        output_ext: output_ext,
-        name:       Utils.slugify(basename_without_ext),
-        title:      Utils.slugify(data['slug']) || Utils.slugify(basename_without_ext)
-      }
+      @url_placeholders ||= Drops::UrlDrop.new(self)
     end
 
     # The permalink for this Document.
@@ -150,10 +199,14 @@ module Bunto
     # Returns the computed URL for the document.
     def url
       @url = URL.new({
-        template:     url_template,
-        placeholders: url_placeholders,
-        permalink:    permalink
+        :template => url_template,
+        :placeholders => url_placeholders,
+        :permalink => permalink
       }).to_s
+    end
+
+    def [](key)
+      data[key]
     end
 
     # The full path to the output file.
@@ -165,7 +218,7 @@ module Bunto
       dest = site.in_dest_dir(base_directory)
       path = site.in_dest_dir(dest, URL.unescape_path(url))
       path = File.join(path, "index.html") if url.end_with?("/")
-      path << output_ext unless path.end_with?(output_ext)
+      path << output_ext unless path.end_with? output_ext
       path
     end
 
@@ -180,16 +233,8 @@ module Bunto
       File.open(path, 'wb') do |f|
         f.write(output)
       end
-    end
 
-    # Returns merged option hash for File.read of self.site (if exists)
-    # and a given param
-    #
-    # opts - override options
-    #
-    # Return the file read options hash.
-    def merged_file_read_opts(opts)
-      site ? site.file_read_opts.merge(opts) : opts
+      trigger_hooks(:post_write)
     end
 
     # Whether the file is published or not, as indicated in YAML front-matter
@@ -205,46 +250,86 @@ module Bunto
     #
     # Returns nothing.
     def read(opts = {})
+      Bunto.logger.debug "Reading:", relative_path
+
       if yaml_file?
         @data = SafeYAML.load_file(path)
       else
         begin
           defaults = @site.frontmatter_defaults.all(url, collection.label.to_sym)
-          unless defaults.empty?
-            @data = defaults
-          end
-          @content = File.read(path, merged_file_read_opts(opts))
+          merge_data!(defaults, source: "front matter defaults") unless defaults.empty?
+
+          self.content = File.read(path, Utils.merged_file_read_opts(site, opts))
           if content =~ YAML_FRONT_MATTER_REGEXP
-            @content = $POSTMATCH
-            data_file = SafeYAML.load($1)
-            unless data_file.nil?
-              @data = Utils.deep_merge_hashes(defaults, data_file)
-            end
+            self.content = $POSTMATCH
+            data_file = SafeYAML.load(Regexp.last_match(1))
+            merge_data!(data_file, source: "YAML front matter") if data_file
           end
+
+          post_read
         rescue SyntaxError => e
-          puts "YAML Exception reading #{path}: #{e.message}"
+          Bunto.logger.error "Error:", "YAML Exception reading #{path}: #{e.message}"
         rescue Exception => e
-          puts "Error reading file #{path}: #{e.message}"
+          if e.is_a? Bunto::Errors::FatalException
+            raise e
+          end
+          Bunto.logger.error "Error:", "could not read file #{path}: #{e.message}"
         end
       end
+    end
+
+    def post_read
+      if relative_path =~ DATE_FILENAME_MATCHER
+        date, slug, ext = $2, $3, $4
+        if !data['date'] || data['date'].to_i == site.time.to_i
+          merge_data!({"date" => date}, source: "filename")
+        end
+      elsif relative_path =~ DATELESS_FILENAME_MATCHER
+        slug, ext = $2, $3
+      end
+
+      # Try to ensure the user gets a title.
+      data["title"] ||= Utils.titleize_slug(slug)
+      # Only overwrite slug & ext if they aren't specified.
+      data['slug'] ||= slug
+      data['ext']  ||= ext
+
+      populate_categories
+      populate_tags
+      generate_excerpt
+    end
+
+    # Add superdirectories of the special_dir to categories.
+    # In the case of es/_posts, 'es' is added as a category.
+    # In the case of _posts/es, 'es' is NOT added as a category.
+    #
+    # Returns nothing.
+    def categories_from_path(special_dir)
+      superdirs = relative_path.sub(/#{special_dir}(.*)/, '').split(File::SEPARATOR).reject do |c|
+        c.empty? || c.eql?(special_dir) || c.eql?(basename)
+      end
+      merge_data!({ 'categories' => superdirs }, source: "file path")
+    end
+
+    def populate_categories
+      merge_data!({
+        'categories' => (
+          Array(data['categories']) + Utils.pluralized_array_from_hash(data, 'category', 'categories')
+        ).map(&:to_s).flatten.uniq
+      })
+    end
+
+    def populate_tags
+      merge_data!({
+        "tags" => Utils.pluralized_array_from_hash(data, "tag", "tags").flatten
+      })
     end
 
     # Create a Liquid-understandable version of this Document.
     #
     # Returns a Hash representing this Document's data.
     def to_liquid
-      if data.is_a?(Hash)
-        Utils.deep_merge_hashes data, {
-          "output"        => output,
-          "content"       => content,
-          "relative_path" => relative_path,
-          "path"          => relative_path,
-          "url"           => url,
-          "collection"    => collection.label
-        }
-      else
-        data
-      end
+      @to_liquid ||= Drops::DocumentDrop.new(self)
     end
 
     # The inspect string for this document.
@@ -259,7 +344,7 @@ module Bunto
     #
     # Returns the content of the document
     def to_s
-      content || ''
+      output || content || 'NO CONTENT'
     end
 
     # Compare this document against another document.
@@ -267,8 +352,11 @@ module Bunto
     #
     # Returns -1, 0, +1 or nil depending on whether this doc's path is less than,
     #   equal or greater than the other doc's path. See String#<=> for more details.
-    def <=>(anotherDocument)
-      path <=> anotherDocument.path
+    def <=>(other)
+      return nil unless other.respond_to?(:data)
+      cmp = data['date'] <=> other.data['date']
+      cmp = path <=> other.path if cmp.nil? || cmp == 0
+      cmp
     end
 
     # Determine whether this document should be written.
@@ -280,5 +368,77 @@ module Bunto
       collection && collection.write?
     end
 
+    # The Document excerpt_separator, from the YAML Front-Matter or site
+    # default excerpt_separator value
+    #
+    # Returns the document excerpt_separator
+    def excerpt_separator
+      (data['excerpt_separator'] || site.config['excerpt_separator']).to_s
+    end
+
+    # Whether to generate an excerpt
+    #
+    # Returns true if the excerpt separator is configured.
+    def generate_excerpt?
+      !excerpt_separator.empty?
+    end
+
+    def next_doc
+      pos = collection.docs.index { |post| post.equal?(self) }
+      if pos && pos < collection.docs.length - 1
+        collection.docs[pos + 1]
+      else
+        nil
+      end
+    end
+
+    def previous_doc
+      pos = collection.docs.index { |post| post.equal?(self) }
+      if pos && pos > 0
+        collection.docs[pos - 1]
+      else
+        nil
+      end
+    end
+
+    def trigger_hooks(hook_name, *args)
+      Bunto::Hooks.trigger collection.label.to_sym, hook_name, self, *args if collection
+      Bunto::Hooks.trigger :documents, hook_name, self, *args
+    end
+
+    def id
+      @id ||= File.join(File.dirname(url), (data['slug'] || basename_without_ext).to_s)
+    end
+
+    # Calculate related posts.
+    #
+    # Returns an Array of related Posts.
+    def related_posts
+      Bunto::RelatedPosts.new(self).build
+    end
+
+    # Override of normal respond_to? to match method_missing's logic for
+    # looking in @data.
+    def respond_to?(method, include_private = false)
+      data.key?(method.to_s) || super
+    end
+
+    # Override of method_missing to check in @data for the key.
+    def method_missing(method, *args, &blck)
+      if data.key?(method.to_s)
+        Bunto.logger.warn "Deprecation:", "Document##{method} is now a key in the #data hash."
+        Bunto.logger.warn "", "Called by #{caller.first}."
+        data[method.to_s]
+      else
+        super
+      end
+    end
+
+    private # :nodoc:
+    def generate_excerpt
+      if generate_excerpt?
+        data["excerpt"] ||= Bunto::Excerpt.new(self)
+      end
+    end
   end
 end
