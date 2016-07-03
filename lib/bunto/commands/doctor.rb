8afcef18e034f456221c35d4dd6eac6dba71a7fc
suriyaa <isc.suriyaa@gmail.com>
2016-07-03 12:08:53 +0200

    Run Site#generate for 'bunto doctor' to catch plugin issues.

--- lib/bunto/commands/doctor.rb
+++ lib/bunto/commands/doctor.rb
@@ -4,11 +4,12 @@ module Bunto
       class << self
         def init_with_program(prog)
           prog.command(:doctor) do |c|
-            c.syntax 'doctor'
-            c.description 'Search site and print specific deprecation warnings'
+            c.syntax "doctor"
+            c.description "Search site and print specific deprecation warnings"
             c.alias(:hyde)
 
-            c.option 'config', '--config CONFIG_FILE[,CONFIG_FILE2,...]', Array, 'Custom configuration file'
+            c.option "config", "--config CONFIG_FILE[,CONFIG_FILE2,...]", Array,
+                            "Custom configuration file"
 
             c.action do |_, options|
               Bunto::Commands::Doctor.process(options)
@@ -18,7 +19,9 @@ module Bunto
 
         def process(options)
           site = Bunto::Site.new(configuration_from_options(options))
+          site.reset
           site.read
+          site.generate
 
           if healthy?(site)
             Bunto.logger.info "Your test results", "are in. Everything looks fine."
@@ -37,7 +40,7 @@ module Bunto
         end
 
         def deprecated_relative_permalinks(site)
-          if site.config['relative_permalinks']
+          if site.config["relative_permalinks"]
             Bunto::Deprecator.deprecation_message "Your site still uses relative" \
                                 " permalinks, which was removed in" \
                                 " Bunto v3.0.0."
@@ -62,7 +65,7 @@ module Bunto
         def fsnotify_buggy?(_site)
           return true unless Utils::Platforms.osx?
           if Dir.pwd != `pwd`.strip
-            Bunto.logger.error "  " + <<-STR.strip.gsub(/\n\s+/, "\n  ")
+            Bunto.logger.error "  " + <<-STR.strip.gsub(%r!\n\s+!, "\n  ")
               We have detected that there might be trouble using fsevent on your
               operating system, you can read https://github.com/thibaudgg/rb-fsevent/wiki/no-fsevents-fired-(OSX-bug)
               for possible work arounds or you can work around it immediately
@@ -78,7 +81,7 @@ module Bunto
         def urls_only_differ_by_case(site)
           urls_only_differ_by_case = false
           urls = case_insensitive_urls(site.pages + site.docs_to_write, site.dest)
-          urls.each do |case_insensitive_url, real_urls|
+          urls.each do |_case_insensitive_url, real_urls|
             next unless real_urls.uniq.size > 1
             urls_only_differ_by_case = true
             Bunto.logger.warn "Warning:", "The following URLs only differ" \
@@ -102,10 +105,9 @@ module Bunto
         end
 
         def case_insensitive_urls(things, destination)
-          things.inject({}) do |memo, thing|
+          things.each_with_object({}) do |thing, memo|
             dest = thing.destination(destination)
             (memo[dest.downcase] ||= []) << dest
-            memo
           end
         end
       end
