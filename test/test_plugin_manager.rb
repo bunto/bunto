1822e98ef3222619a3d3490026ce696a9bd9c7e1
suriyaa <isc.suriyaa@gmail.com>
2016-08-29 15:12:29 +0200

    replace ssert false, ... with 'refute'

--- test/test_plugin_manager.rb
+++ test/test_plugin_manager.rb
@@ -21,7 +21,7 @@ class TestPluginManager < BuntoUnitTest
   context "BUNTO_NO_BUNDLER_REQUIRE set to `true`" do
     should "not require from bundler" do
       with_env("BUNTO_NO_BUNDLER_REQUIRE", "true") do
-        assert_equal false, Bunto::PluginManager.require_from_bundler,
+        refute Bunto::PluginManager.require_from_bundler,
                      "Gemfile plugins were required but shouldn't have been"
         assert ENV["BUNTO_NO_BUNDLER_REQUIRE"]
       end
@@ -32,7 +32,7 @@ class TestPluginManager < BuntoUnitTest
     should "not require from bundler" do
       with_env("BUNTO_NO_BUNDLER_REQUIRE", nil) do
         with_no_gemfile do
-          assert_equal false, Bunto::PluginManager.require_from_bundler,
+          refute Bunto::PluginManager.require_from_bundler,
                        "Gemfile plugins were required but shouldn't have been"
           assert_nil ENV["BUNTO_NO_BUNDLER_REQUIRE"]
         end
