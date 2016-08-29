7bed5f6dabd1995b7b05bef7c36720c267b4048e
suriyaa <isc.suriyaa@gmail.com>
2016-08-29 15:06:13 +0200

    migrate existing tests to should syntax

--- test/test_plugin_manager.rb
+++ test/test_plugin_manager.rb
@@ -8,28 +8,34 @@ class TestPluginManager < BuntoUnitTest
     FileUtils.mv "Gemfile.old", "Gemfile"
   end
 
-  def test_requiring_from_bundler
-    with_env("BUNTO_NO_BUNDLER_REQUIRE", nil) do
-      assert Bunto::PluginManager.require_from_bundler,
-             "require_from_bundler should return true."
-      assert ENV["BUNTO_NO_BUNDLER_REQUIRE"], "Gemfile plugins were not required."
+  context "BUNTO_NO_BUNDLER_REQUIRE set to `nil`" do
+    should "require from bundler" do
+      with_env("BUNTO_NO_BUNDLER_REQUIRE", nil) do
+        assert Bunto::PluginManager.require_from_bundler,
+               "require_from_bundler should return true."
+        assert ENV["BUNTO_NO_BUNDLER_REQUIRE"], "Gemfile plugins were not required."
+      end
     end
   end
 
-  def test_blocking_requiring_from_bundler
-    with_env("BUNTO_NO_BUNDLER_REQUIRE", "true") do
-      assert_equal false, Bunto::PluginManager.require_from_bundler,
-                   "Gemfile plugins were required but shouldn't have been"
-      assert ENV["BUNTO_NO_BUNDLER_REQUIRE"]
-    end
-  end
-
-  def test_no_gemfile
-    with_env("BUNTO_NO_BUNDLER_REQUIRE", nil) do
-      with_no_gemfile do
+  context "BUNTO_NO_BUNDLER_REQUIRE set to `true`" do
+    should "not require from bundler" do
+      with_env("BUNTO_NO_BUNDLER_REQUIRE", "true") do
         assert_equal false, Bunto::PluginManager.require_from_bundler,
                      "Gemfile plugins were required but shouldn't have been"
-        assert_nil ENV["BUNTO_NO_BUNDLER_REQUIRE"]
+        assert ENV["BUNTO_NO_BUNDLER_REQUIRE"]
+      end
+    end
+  end
+
+  context "BUNTO_NO_BUNDLER_REQUIRE set to `nil` and no Gemfile present" do
+    should "not require from bundler" do
+      with_env("BUNTO_NO_BUNDLER_REQUIRE", nil) do
+        with_no_gemfile do
+          assert_equal false, Bunto::PluginManager.require_from_bundler,
+                       "Gemfile plugins were required but shouldn't have been"
+          assert_nil ENV["BUNTO_NO_BUNDLER_REQUIRE"]
+        end
       end
     end
   end
